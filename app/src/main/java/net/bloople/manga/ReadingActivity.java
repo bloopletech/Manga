97389da36dda8856f69dcf783de50c2badf4e2f0
Brenton Fletcher <i@bloople.net>
2016-07-09 04:21:45 +1000

    Don't destroy current image until new onew has loaded

--- app/src/main/java/net/bloople/manga/ReadingActivity.java
+++ app/src/main/java/net/bloople/manga/ReadingActivity.java
@@ -4,16 +4,22 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 public class ReadingActivity extends AppCompatActivity {
     private Book book;
     private int currentPage;
-    private ImageView imageView;
+    private RelativeLayout holder;
+    private RequestListener requestListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +32,9 @@ public class ReadingActivity extends AppCompatActivity {
         book = MangaApplication.books.get(key);
         currentPage = 0;
 
-        imageView = (ImageView)findViewById(R.id.image_view);
+        holder = (RelativeLayout)findViewById(R.id.image_view_holder);
 
-        imageView.setOnClickListener(new View.OnClickListener() {
+        holder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(currentPage == book.pages() - 1) return;
@@ -37,11 +43,32 @@ public class ReadingActivity extends AppCompatActivity {
             }
         });
 
+        requestListener = new RequestListener<Uri, GlideDrawable>() {
+            @Override
+            public boolean onException(Exception e, Uri model, Target<GlideDrawable> target,
+                                       boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(GlideDrawable resource, Uri model, Target<GlideDrawable> target,
+                                           boolean isFromMemoryCache, boolean isFirstResource) {
+                for(int i = 0; i < (holder.getChildCount() - 1); i++) holder.removeViewAt(0);
+                return false;
+            }
+        };
+
         showCurrentPage();
     }
 
     private void showCurrentPage() {
-        Glide.with(this).load(book.pageUrl(currentPage)).dontAnimate().into(imageView);
+        ImageView imageView = (ImageView)LayoutInflater.from(this)
+                .inflate(R.layout.reading_image_view, holder, false);
+
+        holder.addView(imageView);
+
+        Glide.with(this).load(book.pageUrl(currentPage))
+                .diskCacheStrategy(DiskCacheStrategy.ALL).dontAnimate().listener(requestListener).into(imageView);
 
         if ((currentPage + 1) < book.pages()) {
             Glide.with(this).load(book.pageUrl(currentPage + 1)).diskCacheStrategy(DiskCacheStrategy.SOURCE).preload();
