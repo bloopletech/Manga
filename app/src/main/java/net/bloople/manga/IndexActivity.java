a463f6b924842b1269113c3a2b5b0dffe4206da0
Brenton Fletcher <i@bloople.net>
2016-07-12 20:46:46 +1000

    Prompt user before exiting app (via back button)

--- app/src/main/java/net/bloople/manga/IndexActivity.java
+++ app/src/main/java/net/bloople/manga/IndexActivity.java
@@ -1,5 +1,7 @@
 package net.bloople.manga;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
@@ -31,6 +33,20 @@ public class IndexActivity extends AppCompatActivity {
             LoadBooksTask loader = new LoadBooksTask();
             loader.execute();
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        IndexActivity.this.finish();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 
     private void initAdapter() {
