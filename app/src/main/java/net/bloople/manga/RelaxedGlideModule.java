22dd1b88201b302210945cfea944c7cfb8bace8d
Brenton Fletcher <i@bloople.net>
2017-07-26 00:48:31 +1000

    Update compileSdkVersion to latest; Upgrade dependencies

--- app/src/main/java/net/bloople/manga/RelaxedGlideModule.java
+++ app/src/main/java/net/bloople/manga/RelaxedGlideModule.java
@@ -4,10 +4,11 @@ import android.content.Context;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
-import com.bumptech.glide.integration.okhttp.OkHttpUrlLoader;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.GlideModule;
-import com.squareup.okhttp.OkHttpClient;
+
+import okhttp3.OkHttpClient;
 
 import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
@@ -17,7 +18,7 @@ import java.util.concurrent.TimeUnit;
  */
 
 public class RelaxedGlideModule implements GlideModule {
-    public static final int TIMEOUT_SECONDS = 60;
+    static final int TIMEOUT_SECONDS = 60;
 
     @Override
     public void applyOptions(Context context, GlideBuilder builder) {
@@ -27,13 +28,12 @@ public class RelaxedGlideModule implements GlideModule {
     @Override
     public void registerComponents(Context context, Glide glide) {
         // register ModelLoaders here.
-        OkHttpClient client = new OkHttpClient();
-        client.setConnectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS);
-        client.setReadTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS);
-        client.setWriteTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+                .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+                .writeTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+                .build();
 
-        OkHttpUrlLoader.Factory factory = new OkHttpUrlLoader.Factory(client);
-
-        glide.register(GlideUrl.class, InputStream.class, factory);
+        glide.register(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(client));
     }
 }
