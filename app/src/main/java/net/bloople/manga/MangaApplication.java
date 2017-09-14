1884711d2a94529a9d6b9d958397a0647d9e03ad
Brenton Fletcher <i@bloople.net>
2017-09-14 20:24:43 +1000

    Update host URL

--- app/src/main/java/net/bloople/manga/MangaApplication.java
+++ app/src/main/java/net/bloople/manga/MangaApplication.java
@@ -19,6 +19,6 @@ public class MangaApplication extends Application {
     }
 
     public static Uri root() {
-        return Uri.parse("http://192.168.1.2/Manga-OG/.mangos/");
+        return Uri.parse("http://192.168.1.100:82/Manga-OG/.mangos/");
     }
 }
