d3da6ef47472fc50640b9f0b5d1e2047d41bffe8
Brenton "B-Train" Fletcher <i@bloople.net>
2020-12-03 00:52:04 +1100

    Prefer ARGB_8888 over RGB_565, as the latter results in worse image quality

--- app/src/main/java/net/bloople/manga/RelaxedGlideModule.java
+++ app/src/main/java/net/bloople/manga/RelaxedGlideModule.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.GlideModule;
 
@@ -18,12 +19,11 @@ public class RelaxedGlideModule implements GlideModule {
 
     @Override
     public void applyOptions(Context context, GlideBuilder builder) {
-        // Apply options to the builder here.
+        builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
     }
 
     @Override
     public void registerComponents(Context context, Glide glide) {
-        // register ModelLoaders here.
         OkHttpClient client = new OkHttpClient.Builder()
                 .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
                 .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
