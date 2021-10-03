077904842f9506d1c177c73db117ca75fa0d0f91
Brenton "B-Train" Fletcher <i@bloople.net>
2021-10-04 05:14:16 +1100

    Auto-hide the navigation bar when reading a book

--- app/src/main/java/net/bloople/manga/ReadingActivity.java
+++ app/src/main/java/net/bloople/manga/ReadingActivity.java
@@ -3,6 +3,7 @@ package net.bloople.manga;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -73,5 +74,28 @@ public class ReadingActivity extends AppCompatActivity {
         int currentItem = pager.getCurrentItem();
         pager.setAdapter(pager.getAdapter());
         pager.setCurrentItem(currentItem, false);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        // Enables "sticky immersive" mode
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
