8f8e7107d6871cb26011fef9cc5298cf3693db43
Brenton "B-Train" Fletcher <i@bloople.net>
2021-10-30 06:57:26 +1100

    Convert ThrottledOnClickListener Java -> Kotlin

---
+++ app/src/main/java/net/bloople/manga/ThrottledOnClickListener.kt
@@ -0,0 +1,29 @@
+package net.bloople.manga
+
+import android.os.SystemClock
+import android.view.View
+
+internal abstract class ThrottledOnClickListener : View.OnClickListener {
+    private var lastClickMillis: Long = 0
+
+    override fun onClick(v: View) {
+        val now = SystemClock.elapsedRealtime()
+        if(now - lastClickMillis > THRESHOLD_MILLIS) onThrottledClick(v)
+        lastClickMillis = now
+    }
+
+    abstract fun onThrottledClick(v: View?)
+
+    companion object {
+        const val THRESHOLD_MILLIS = 1000L
+
+        @JvmStatic
+        fun wrap(clickListener: View.OnClickListener): View.OnClickListener {
+            return object : ThrottledOnClickListener() {
+                override fun onThrottledClick(v: View?) {
+                    clickListener.onClick(v)
+                }
+            }
+        }
+    }
+}
