355cdb77cbc709c701cc8dba20066c4bbb850aaf
Brenton "B-Train" Fletcher <i@bloople.net>
2021-10-30 07:10:31 +1100

    Convert QueryAdapter Java -> Kotlin

---
+++ app/src/main/java/net/bloople/manga/QueryAdapter.kt
@@ -0,0 +1,30 @@
+package net.bloople.manga
+
+import android.content.Context
+import android.database.Cursor
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.CursorAdapter
+import android.widget.TextView
+
+class QueryAdapter internal constructor(context: Context, cursor: Cursor?) : CursorAdapter(context, cursor, 0) {
+    // The newView method is used to inflate a new view and return it,
+    // you don't bind any data to the view at this point.
+    override fun newView(context: Context, cursor: Cursor, parent: ViewGroup): View {
+        return LayoutInflater.from(context).inflate(R.layout.index_query, parent, false)
+    }
+
+    // The bindView method is used to bind all data to a given view
+    // such as setting the text on a TextView.
+    override fun bindView(view: View, context: Context, cursor: Cursor) {
+        // Find fields to populate in inflated template
+        val textView = view as TextView
+        textView.text = cursor.getString(cursor.getColumnIndex("text"))
+    }
+
+    override fun convertToString(cursor: Cursor): String {
+        //returns string inserted into textview after item from drop-down list is selected.
+        return cursor.getString(cursor.getColumnIndex("text"))
+    }
+}
