c786589f9af51f39f7a5e98145f97716df6eb9f3
Brenton Fletcher <i@bloople.net>
2022-03-15 01:59:07 +1100

    Convert LibrariesAuditor Java -> Kotlin

---
+++ app/src/main/java/net/bloople/manga/audit/LibrariesAuditor.kt
@@ -0,0 +1,20 @@
+package net.bloople.manga.audit
+
+import android.content.Context
+import net.bloople.manga.Library
+
+class LibrariesAuditor(private val context: Context) {
+    fun selected(library: Library) {
+        val event = AuditEvent(
+            System.currentTimeMillis(),
+            Action.LIBRARY_SELECTED,
+            ResourceType.UNKNOWN,
+            AuditEvent.UNKNOWN_ID,
+            ResourceType.LIBRARY,
+            library.id,
+            library.name,
+            ""
+        )
+        event.save(context)
+    }
+}
