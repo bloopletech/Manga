4e96236c5d4ef3c60609197e36c10fdb891d9339
Brenton "B-Train" Fletcher <i@bloople.net>
2022-04-06 02:52:24 +1000

    Handle CharSequence

--- app/src/main/java/net/bloople/manga/DatabaseExtensions.kt
+++ app/src/main/java/net/bloople/manga/DatabaseExtensions.kt
@@ -9,7 +9,7 @@ internal inline operator fun <reified T> Cursor.get(columnName: String): T {
         Int::class -> this.getInt(this.getColumnIndexOrThrow(columnName)) as T
         Long::class -> this.getLong(this.getColumnIndexOrThrow(columnName)) as T
         Short::class -> this.getShort(this.getColumnIndexOrThrow(columnName)) as T
-        String::class -> this.getString(this.getColumnIndexOrThrow(columnName)) as T
+        String::class, CharSequence::class -> this.getString(this.getColumnIndexOrThrow(columnName)) as T
         else -> throw IllegalArgumentException("Cursor does not have a getter for type ${T::class.qualifiedName}")
     }
 }
